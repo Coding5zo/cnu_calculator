0126121667a83ebd86c1189e013e084dbaa1197b
rhh0109 <rhh0109@naver.com>
2017-04-06 18:27:01 +0900

    add Calculate class infixToPostfix method

--- cnu_calculator_5zo/src/Calculate.java
+++ cnu_calculator_5zo/src/Calculate.java
@@ -26,7 +26,61 @@ public class Calculate {
 	}
 
 	public boolean infixToPostfix() {
-		
+		int i;
+		int p;
+		char curToken, poppedToken, topToken;
+		char tempToken = '\0';
+		this._oStack = new ArrayList<Character>(this._infix.length);
+		this._postfix = new String[this._infix.length];
+		i = 0;
+		p = 0;
+
+		while (i < this._infix.length) {
+			curToken = this._infix[i++];
+			if (isdigit(curToken)) {
+				if(isdigit(tempToken)){
+					this._postfix[p - 1] = this._postfix[p - 1] + curToken;
+				} else {
+					this._postfix[p++] = String.valueOf(curToken);
+				}
+				tempToken = curToken;
+			} else {
+				if (curToken == ')') { 
+					if (!this._oStack.isEmpty()) {
+						poppedToken = (char) this._oStack.pop();
+					} else
+						return false; 
+
+					while (poppedToken != '(') {
+						this._postfix[p++] = String.valueOf(poppedToken);
+						if (!this._oStack.isEmpty()) {
+							poppedToken = (char) this._oStack.pop();
+						} else
+							return false; 
+					}
+					tempToken = '\0';
+				} else { 
+					int inComingP = inComingPrecedence(curToken);
+					if (!this._oStack.isEmpty()) {
+						topToken = (char) _oStack.peek();
+						while (inStackPrecedence(topToken) >= inComingP) {
+							poppedToken = (char) this._oStack.pop();
+							this._postfix[p++] = String.valueOf(poppedToken);
+							if (!this._oStack.isEmpty())
+								topToken = (char) this._oStack.peek();
+							else
+								break;
+						}
+					}
+					this._oStack.push(curToken);
+					tempToken = '\0';
+				}
+			}
+		} 
+		while (!this._oStack.isEmpty()) {
+			this._postfix[p++] = String.valueOf(this._oStack.pop());
+		}
+		return true;
 	}
 
 	public double evalPostfix() {
