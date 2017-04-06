950d2f8ea669fc69ac127c6d97b4e88131d3de4c
rhh0109 <rhh0109@naver.com>
2017-04-06 18:18:24 +0900

    add Calculate class methods interface

--- cnu_calculator_5zo/src/Calculate.java
+++ cnu_calculator_5zo/src/Calculate.java
@@ -3,4 +3,45 @@ public class Calculate {
 	private ArrayList<Double> _vStack;
 	private char[] _infix;
 	private String[] _postfix;
+
+	public Calculate() {
+		
+	}
+	public void setInfix(String anInfix) {
+		
+	}
+
+	public String infix() {
+		
+	}
+
+	public String postfix() {
+		
+	}
+
+	public boolean infixToPostfix() {
+		
+	}
+
+	public double evalPostfix() {
+		
+	}
+
+	
+
+
+	private boolean isdigit(char aToken) {
+		
+
+	}
+	private boolean isdigit(String aToken) {
+		
+	}
+	private int inComingPrecedence(char aToken) {
+		
+	}
+
+	private int inStackPrecedence(char aToken) {
+
+	}
 }
