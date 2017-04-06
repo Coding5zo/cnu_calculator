b34118088204494a4224a87769bc192fde4b3c39
sentendro <ghu333@hanmail.net>
2017-04-06 18:35:52 +0900

    add constructor on AppController

--- cnu_calculator_5zo/src/AppController.java
+++ cnu_calculator_5zo/src/AppController.java
@@ -1,4 +1,9 @@
 public class AppController {
 	private AppView _appView;
-	private Calculate _caclulate;
+	private Calculate _calclulate;
+	
+	public AppController(){
+		this._appView = new AppView();
+		this._calclulate = new Calculate();
+	}
 }
